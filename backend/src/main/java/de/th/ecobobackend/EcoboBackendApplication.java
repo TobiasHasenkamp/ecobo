3ac7d395be0ba5d3e0b89d7700959fece7d454c4
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-11-14 14:31:09 +0100

    Setup of the java backend.

---
+++ backend/src/main/java/de/th/ecobobackend/EcoboBackendApplication.java
@@ -0,0 +1,13 @@
+package de.th.ecobobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoboBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoboBackendApplication.class, args);
+	}
+
+}
