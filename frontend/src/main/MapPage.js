42419a435aa44628212d37d4fcc4c93942aa658f
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-11-16 08:55:04 +0100

    Added the map placeholder in MapPage.js. The way the StyledDarkGreenBox-Element is used right now should be changed (outsource into an own component with the white gradient elements in it).

--- frontend/src/main/MapPage.js
+++ frontend/src/main/MapPage.js
@@ -1,5 +1,6 @@
 import PageHeader from "./PageHeader";
 import React from "react";
+import styled from "styled-components/macro";
 
 export default function MapPage() {
 
@@ -7,8 +8,29 @@ export default function MapPage() {
 
         <div>
             <PageHeader title="EcoMap"/>
-            <p>Bochum Map page</p>
+            <StyledMapPlaceholder>Map placeholder</StyledMapPlaceholder>
+            <StyledDarkGreenBox/>
         </div>
 
     );
 }
+
+
+const StyledMapPlaceholder = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ghostwhite;
+  background-color: steelblue;
+  height: 425px;
+  margin-top: var(--m);
+  margin-bottom: var(--m);
+  width: 100%;
+`
+
+const StyledDarkGreenBox = styled.div`
+  display: block;
+  width: 100%;
+  background-color: var(--darkgreen);
+  height: 75px;
+`
