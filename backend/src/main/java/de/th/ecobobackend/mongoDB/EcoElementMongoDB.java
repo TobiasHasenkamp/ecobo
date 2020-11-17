f08178f168ee7999e7045f7fb0aa98ba5ae6513c
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-11-17 13:21:33 +0100

    Added some Unit tests for the EcoElementService.java. An EcoElementSeeder helps to create EcoElements for the tests.

--- backend/src/main/java/de/th/ecobobackend/mongoDB/EcoElementMongoDB.java
+++ backend/src/main/java/de/th/ecobobackend/mongoDB/EcoElementMongoDB.java
@@ -1,16 +1,21 @@
 package de.th.ecobobackend.mongoDB;
 
 import de.th.ecobobackend.model.EcoElement;
+import de.th.ecobobackend.model.enums.Category;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EcoElementMongoDB extends PagingAndSortingRepository<EcoElement, String> {
 
     @Override
     List<EcoElement> findAll();
 
-    @Override
-    List<EcoElement> findAll(Sort sort);
+    Optional<EcoElement> findByName(String name);
+
+    Optional<List<EcoElement>> findAllByCategory(Category category);
+
+
 }
--- backend/src/main/java/de/th/ecobobackend/service/EcoElementService.java
+++ backend/src/main/java/de/th/ecobobackend/service/EcoElementService.java
@@ -2,11 +2,15 @@ package de.th.ecobobackend.service;
 
 import de.th.ecobobackend.model.EcoElement;
 import de.th.ecobobackend.model.dto.EcoElementDto;
+import de.th.ecobobackend.model.enums.Category;
 import de.th.ecobobackend.mongoDB.EcoElementMongoDB;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EcoElementService {
@@ -29,10 +33,36 @@ public class EcoElementService {
         return ecoElementMongoDB.findAll();
     }
 
+    public EcoElement findById(String id){
+        Optional<EcoElement> result = ecoElementMongoDB.findById(id);
+        if (result.isPresent()){
+            return result.get();
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    public EcoElement findByName(String name){
+        Optional<EcoElement> result = ecoElementMongoDB.findByName(name);
+        if (result.isPresent()){
+            return result.get();
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    public List<EcoElement> findAllByCategory(Category category){
+        Optional<List<EcoElement>> result = ecoElementMongoDB.findAllByCategory(category);
+        if (result.isPresent()){
+            return result.get();
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    //not tested yet
     public EcoElement addEcoElement(EcoElementDto ecoElementDto){
         return ecoElementMongoDB.save(ecoElementBuilder.build(ecoElementDto));
     }
 
+    //not tested yet
     public EcoElement addRandomEcoElement() {
         return ecoElementMongoDB.save(ecoElementBuilder.buildRandomElement());
     }
