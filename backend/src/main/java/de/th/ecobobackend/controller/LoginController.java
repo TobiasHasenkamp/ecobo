59f83cda070d70c2584781005a2dfaeaccb7f722
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-11-23 16:31:29 +0100

    Added a RegistrationController and some necessary methods to the UserProfileService to allow the registration of new users at "/auth/registration".

--- backend/src/main/java/de/th/ecobobackend/controller/LoginController.java
+++ backend/src/main/java/de/th/ecobobackend/controller/LoginController.java
@@ -41,8 +41,9 @@ public class LoginController {
         }
 
         return jwtUtils.createToken(loginDto.getUsername(), new HashMap<>());
-
     }
+
+
 
 
 }
---
+++ backend/src/main/java/de/th/ecobobackend/service/UserProfileService.java
@@ -0,0 +1,54 @@
+package de.th.ecobobackend.service;
+
+import de.th.ecobobackend.model.UserProfile;
+import de.th.ecobobackend.model.dto.UserLoginDto;
+import de.th.ecobobackend.mongoDB.UserProfileMongoDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserProfileService {
+
+    private final UserProfileMongoDB userProfileMongoDB;
+
+    @Autowired
+    public UserProfileService(UserProfileMongoDB userProfileMongoDB) {
+        this.userProfileMongoDB = userProfileMongoDB;
+    }
+
+    //============================================================================
+    //main methods
+    //============================================================================
+
+    public List<UserProfile> listAllUsers(){
+        return userProfileMongoDB.findAll();
+    }
+
+    public boolean isUsernameOccupied(String username){
+        return userProfileMongoDB.existsById(username);
+    }
+
+    public UserProfile getUserDataByUsername(String username){
+        Optional<UserProfile> user = userProfileMongoDB.findById(username);
+        if (user.isPresent()){
+            return user.get();
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    public UserProfile registerNewUser(UserLoginDto userLoginDto, String encryptedPassword){
+
+        UserProfile newUser = UserProfile.builder()
+                        .username(userLoginDto.getUsername())
+                        .password(encryptedPassword)
+                        .build();
+
+        return userProfileMongoDB.save(newUser);
+    }
+
+}
