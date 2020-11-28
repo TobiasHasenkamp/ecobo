0b4d8ef5275f0e91bc88d5274417d4a2765328c7
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-11-28 16:53:15 +0100

    The list boxes at ListPages.js now also have the icon/button to close and open the box.

--- frontend/src/main/list-route/FairShopList.js
+++ frontend/src/main/list-route/FairShopList.js
@@ -1,17 +1,32 @@
 import ShowElementIconButton from "../designElements/buttons/ShowElementIconButton";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {StyledWrapperTable, StyledHeaderRow, StyledElement, StyledElementHeader, StyledNameCell,
     StyledCell, StyledIconDiv, StyledElementBody} from "./StyledElementsForTableDesign";
+import {FaRegArrowAltCircleDown, FaRegArrowAltCircleUp} from "react-icons/fa";
 
 export default function FairShopList({ecoElements}){
+
+    const [fairShopTableIsOpen, setFairShopTableIsOpen] = useState(true);
+
+    function handleShowFairShopTable(){
+        setFairShopTableIsOpen(!fairShopTableIsOpen);
+    }
+
+    useEffect(() => {
+        setFairShopTableIsOpen(true);
+    }, [])
 
     return (
 
         <StyledWrapperTable name="Weltläden">
             <StyledHeaderRow className="yellow">
-                Weltläden
+                {"Weltläden "}
+                {fairShopTableIsOpen ? <FaRegArrowAltCircleUp style={{fontSize: "0.9em", marginBottom: "-1px"}} onClick={handleShowFairShopTable}/>
+                    : <FaRegArrowAltCircleDown style={{fontSize: "0.9em", marginBottom: "-1px"}} onClick={handleShowFairShopTable}/>}
+
             </StyledHeaderRow>
             {
+                fairShopTableIsOpen &&
                 ecoElements?.filter(element => element.category === "FAIRSHOP").map((element) => (
                     <StyledElement key={element.id}>
                         <div/>
--- frontend/src/main/list-route/FoodStoreList.js
+++ frontend/src/main/list-route/FoodStoreList.js
@@ -1,17 +1,30 @@
 import ShowElementIconButton from "../designElements/buttons/ShowElementIconButton";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {StyledWrapperTable, StyledHeaderRow, StyledElement, StyledElementHeader, StyledNameCell,
     StyledCell, StyledIconDiv, StyledElementBody} from "./StyledElementsForTableDesign";
+import {FaRegArrowAltCircleDown, FaRegArrowAltCircleUp} from "react-icons/fa";
 
 export default function FoodStoreList({ecoElements}){
+
+    const [foodStoreTableIsOpen, setFoodStoreTableIsOpen] = useState(true);
+
+    function handleShowFoodStoreTable(){
+        setFoodStoreTableIsOpen(!foodStoreTableIsOpen);
+    }
+
+    useEffect(() => {
+        setFoodStoreTableIsOpen(true);
+    }, [])
 
     return (
 
             <StyledWrapperTable name="Bioläden">
                 <StyledHeaderRow className="red">
-                    Bioläden
+                    {"Bioläden "}
+                    {foodStoreTableIsOpen ? <FaRegArrowAltCircleUp style={{fontSize: "0.9em", marginBottom: "-1px"}} onClick={handleShowFoodStoreTable}/>
+                        : <FaRegArrowAltCircleDown style={{fontSize: "0.9em", marginBottom: "-1px"}} onClick={handleShowFoodStoreTable}/>}
                 </StyledHeaderRow>
-                {
+                { foodStoreTableIsOpen &&
                     ecoElements?.filter(element => element.category === "FOODSTORE").map((element) => (
                         <StyledElement key={element.id}>
                             <div/>
