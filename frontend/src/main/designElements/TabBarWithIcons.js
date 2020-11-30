611dfc6ec44233b12273dd14d346e79ea246951f
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-11-30 14:05:49 +0100

    Added the main frontend functionality to add filters without the actual filtering method that sorts out the EcoElements. At the moment the feature is only working at the EcoList.

--- frontend/src/main/designElements/TabBarWithIcons.js
+++ frontend/src/main/designElements/TabBarWithIcons.js
@@ -1,13 +1,22 @@
 import styled from "styled-components/macro";
-import React from "react";
+import React, {useState} from "react";
 import ShowsAsListIconButton from "./buttons/ShowsAsListIconButton";
 import ShowAsMapIconButton from "./buttons/ShowAsMapIconButton";
 import ShowAsCardsIconButton from "./buttons/ShowAsCardsIconButton"
 import {useHistory} from "react-router-dom"
+import {FaRegArrowAltCircleDown} from "react-icons/fa";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
 
 export default function TabBarWithIcons({type}) {
 
     const history = useHistory();
+    const [filterBarIsOpen, setFilterBarisOpen] = useState(false);
+    const [categoryMenuStatusAndAnchor, setCategoryMenuStatusAndAnchor] = useState(null);
+    const [certificatesMenuStatusAndAnchor, setCertificatesMenuStatusAndAnchor] = useState(null);
+    const [locationMenuStatusAndAnchor, setLocationMenuStatusAndAnchor] = useState(null);
+    const [filterList, setFilterList] = useState([]);
 
     function handleLinkToMap() {
             history.push("/bo/map");
@@ -21,13 +30,140 @@ export default function TabBarWithIcons({type}) {
         history.push("/404");
     }
 
+    function handleOpenFilterBar(){
+        setFilterBarisOpen(!filterBarIsOpen);
+    }
+
+    function handleCloseCategoryFilterMenu(){
+        setCategoryMenuStatusAndAnchor(null);
+    }
+
+    function handleOpenCategoryFilterMenu(event){
+        setCategoryMenuStatusAndAnchor(event.currentTarget)
+    }
+
+    function handleOpenCertificatesFilterMenu(event){
+        setCertificatesMenuStatusAndAnchor(event.currentTarget)
+    }
+
+    function handleOpenLocationFilterMenu(event){
+        setLocationMenuStatusAndAnchor(event.currentTarget)
+    }
+
+    function handleCloseCertificatesFilterMenu(){
+        setCertificatesMenuStatusAndAnchor(null);
+    }
+
+    function handleCloseLocationFilterMenu(){
+        setLocationMenuStatusAndAnchor(null);
+    }
+
+    function handleRemoveFilter(event){
+        const filterToRemove = event.target.getAttribute("name");
+        const newFilterList = filterList.filter(value => value !== filterToRemove);
+        setFilterList(newFilterList);
+    }
+
+
+    function returnActiveFilter(){
+
+        return(
+
+            filterList.map((element) => (
+                <div key={element} name={element} onClick={handleRemoveFilter}>{element}</div>
+            ))
+
+        )
+    }
+
+
+    function handleAddItemToFilter(event){
+        const newFilterList = filterList;
+        newFilterList.push(event.target.getAttribute("name"));
+        setFilterList(newFilterList);
+        console.log(newFilterList);
+        setCategoryMenuStatusAndAnchor(null);
+        setCertificatesMenuStatusAndAnchor(null);
+        setLocationMenuStatusAndAnchor(null);
+        setFilterBarisOpen(false);
+    }
+
 
     return(
         <>
             <StyledTabBar>
-                {(type !== "map") && <ShowAsMapIconButton handle={handleLinkToMap}/>}
-                {(type !== "list") && <ShowsAsListIconButton handle={handleLinkToList}/>}
-                {(type !== "cards") && <ShowAsCardsIconButton handle={handleLinkToCards}/>}
+
+                <StyledFilterCell>
+
+                    <div onClick={handleOpenFilterBar}>
+                        Filter
+                        <FaRegArrowAltCircleDown onClick={handleOpenFilterBar}
+                                                 style={{fontSize: "0.9em", marginLeft: "4px", marginBottom: "-2px"}}
+                        />
+                    </div>
+
+                </StyledFilterCell>
+
+                <StyledActiveFilterList>
+
+                    {returnActiveFilter()}
+
+                </StyledActiveFilterList>
+
+                <StyledChangeViewCell>
+
+                    {(type !== "map") && <ShowAsMapIconButton handle={handleLinkToMap}/>}
+                    {(type !== "list") && <ShowsAsListIconButton handle={handleLinkToList}/>}
+                    {(type !== "cards") && <ShowAsCardsIconButton handle={handleLinkToCards}/>}
+
+                </StyledChangeViewCell>
+
+                {filterBarIsOpen &&
+                    <StyledFilterBar>
+                        {"nach: "}
+                        <StyledFilterBarMenuButton onClick={handleOpenCategoryFilterMenu}>Typ</StyledFilterBarMenuButton>
+                        <StyledFilterBarMenuButton onClick={handleOpenCertificatesFilterMenu}>Zertifikat</StyledFilterBarMenuButton>
+                        <StyledFilterBarMenuButton onClick={handleOpenLocationFilterMenu}>Ort</StyledFilterBarMenuButton>
+                    </StyledFilterBar>
+                }
+
+                <Menu
+                    id="filterMenuForCategory"
+                    anchorEl={categoryMenuStatusAndAnchor}
+                    keepMounted
+                    open={Boolean(categoryMenuStatusAndAnchor)}
+                    onClose={handleCloseCategoryFilterMenu}
+                >
+                    {!filterList.includes("Bioladen") && <MenuItem name="Bioladen" onClick={handleAddItemToFilter}>Bioladen</MenuItem>}
+                    {!filterList.includes("Restaurant") && <MenuItem name="Restaurant" onClick={handleAddItemToFilter}>Restaurant</MenuItem>}
+                    {!filterList.includes("Weltladen") && <MenuItem name="Weltladen" onClick={handleAddItemToFilter}>Weltladen</MenuItem>}
+                </Menu>
+
+                <Menu
+                    id="filterMenuForCertificates"
+                    anchorEl={certificatesMenuStatusAndAnchor}
+                    keepMounted
+                    open={Boolean(certificatesMenuStatusAndAnchor)}
+                    onClose={handleCloseCertificatesFilterMenu}
+                >
+                    {!filterList.includes("Veganes Angebot") && <MenuItem name="Veganes Angebot" onClick={handleAddItemToFilter}>Veganes Angebot</MenuItem>}
+                    {!filterList.includes("Vegetarisches Angebot") && <MenuItem name="Vegetarisches Angebot" onClick={handleAddItemToFilter}>Vegetarisches Angebot</MenuItem>}
+                    {!filterList.includes("Abholservice") && <MenuItem name="Abholservice" onClick={handleAddItemToFilter}>Abholservice</MenuItem>}
+                </Menu>
+
+                <Menu
+                    id="filterMenuForLocation"
+                    anchorEl={locationMenuStatusAndAnchor}
+                    keepMounted
+                    open={Boolean(locationMenuStatusAndAnchor)}
+                    onClose={handleCloseLocationFilterMenu}
+                >
+                    {!filterList.includes("Innenstadt") && <MenuItem name="Innenstadt" onClick={handleAddItemToFilter}>Innenstadt</MenuItem>}
+                    {!filterList.includes("Weitmar") && <MenuItem name="Weitmar" onClick={handleAddItemToFilter}>Weitmar</MenuItem>}
+                    {!filterList.includes("Riemke") && <MenuItem name="Riemke" onClick={handleAddItemToFilter}>Riemke</MenuItem>}
+                </Menu>
+
+
             </StyledTabBar>
         </>
     )
@@ -36,9 +172,9 @@ export default function TabBarWithIcons({type}) {
 
 
 const StyledTabBar = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin: 7px 15px 3px 5px;
+  display: grid;
+  grid-template-columns: 20% 60% 20%;
+  margin: 7px 15px 3px 25px;
   line-height: 1.4em;
   font-size: 1.2em;
   color: var(--darkgrey);
@@ -53,4 +189,54 @@ const StyledTabBar = styled.div`
          color: var(--lightgrey);
       }
     }
+`
+
+const StyledFilterCell = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  font-size: 0.9em;
+  font-weight: bold;
+  margin-right: 10px;
+  white-space: nowrap;
+`
+
+const StyledChangeViewCell = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const StyledFilterBar = styled.div`
+  font-size: 0.75em;
+  grid-column: 1 / span 3;
+  display: flex;
+  justify-content: space-around;
+  margin: 2px 12vh 2px 2vh;
+`
+
+const StyledFilterBarMenuButton = styled.div`
+  font-weight: bold;
+  font-size: 0.8em;
+  background: lightgray;
+  color: black;
+  padding: 2px 8px;
+  border-radius: 25% 25% 25% 25%;
+  margin-top: -3px;
+`
+
+const StyledActiveFilterList = styled.div`
+  font-size: 0.6em;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  width: auto;
+  margin: 5px;
+  overflow-wrap: anywhere;
+  
+  div {
+      background: lightgray;
+      color: black;
+      padding: 1px 6px;
+      border-radius: 25% 25% 25% 25%;
+      margin: 2px;
+  }
 `
