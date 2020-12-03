49822e183b73cfa9b0eaeade8b62e3dd010bd9c2
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-12-03 13:13:47 +0100

    The MapMarkers in MapMarkersForMap.js are now different and colorful in relation to their category and subcategory. At the moment the Apikey is hardcoded in MapMarkersForMap.js.

--- frontend/src/main/list-map-route/subComponents/MapMarkersForMap.js
+++ frontend/src/main/list-map-route/subComponents/MapMarkersForMap.js
@@ -10,22 +10,82 @@ import ShowElementIconButton from "../../designElements/buttons/ShowElementIconB
 import L from "leaflet";
 
 
-let TestIcon = L.divIcon({
-    html: "<img src='https://api.geoapify.com/v1/icon/?type=awesome&color=%23242f8c&size=large&icon=cloud&iconSize=small&textSize=small&apiKey=7611146e203d46b7a43e78485065f463' alt='marker'/>",
-    iconSize: [0, 0],
-    iconAnchor: [0, 0]
-})
 
 export default function MapMarkersForMap(ecoElements){
 
     const {returnIfItemsGetsFiltered} = useContext(FilterListContext);
 
 
+    function returnMarkerIcon(category, categorySub){
+
+        //later change the apikey into a env-variable, but how?
+        const apikey = "7611146e203d46b7a43e78485065f463";
+        let color;
+        let icon;
+
+        switch(category){
+            case "FOODSTORE": color="23b3200c"
+                break;
+            case "FASHIONSTORE": color="230066cc"
+                break;
+            case "FAIRSHOP": color="23993399"
+                break;
+            case "RESTAURANT": color="23eba434"
+                break;
+            case "OTHER": color="2300802b"
+                break;
+            default: color="2300802b"
+        }
+
+        switch(categorySub){
+            case "FOODSTORE_SUPERMARKET": icon="shopping-cart"
+                break;
+            case "FOODSTORE_NORMAL": icon="store"
+                break;
+            case "FOODSTORE_HEALTHSTORE": icon="store-alt"
+                break;
+            case "FOODSTORE_FARMSHOP": icon="warehouse"
+                break;
+            case "RESTAURANT_SNACKBAR": icon="hamburger"
+                break;
+            case "RESTAURANT_CAFE": icon="coffee"
+                break;
+            case "RESTAURANT_RESTAURANT": icon="utensils"
+                break;
+            case "RESTAURANT_ICECREAM_CAFE": icon="ice-cream"
+                break;
+            case "RESTAURANT_BAKERY": icon="bread-slice"
+                break;
+            case "FAIRSHOP_NORMAL": icon="globe-americas"
+                break;
+            case "FASHIONSTORE_ECO_FASHION_STORE": icon="tshirt"
+                break;
+            case "FASHIONSTORE_SECOND_HAND_STORE": icon="vest"
+                break;
+            case "OTHER": icon="bookmark"
+                break;
+            default: icon="bookmark"
+        }
+
+
+        const iconHtml = "<img src='https://api.geoapify.com/v1/icon/?type=awesome&color=%" +
+                color + "&size=large&icon=" + icon + "&iconSize=small&textSize=small&apiKey=" + apikey + "' alt='marker'/>";
+
+
+        return L.divIcon({
+            html: iconHtml,
+            iconSize: [0, 0],
+            iconAnchor: [0, 0]
+        })
+
+    }
+
+
         return (
 
             ecoElements?.filter(element => (returnIfItemsGetsFiltered(element))).map((element) => (
                 <Marker key={element.id} position={[element.lon, element.lat]}
-                        title={element.name} icon={TestIcon}>
+                        title={element.name} icon={returnMarkerIcon(element.category, element.categorySub)}>
                     <StyledPopup>
                         <StyledPopupHeader>
                             {element.name}
