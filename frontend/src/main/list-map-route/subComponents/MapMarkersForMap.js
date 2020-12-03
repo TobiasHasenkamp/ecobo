358ecbdb2046d8245b72dfca7a81a7da0d4df8cf
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-12-03 09:01:45 +0100

    Small design changes to make the popups from the marker on the map look a little bit more interesting. The popup now offers a link to go to the EcoElementPages.

--- frontend/src/main/list-map-route/subComponents/MapMarkersForMap.js
+++ frontend/src/main/list-map-route/subComponents/MapMarkersForMap.js
@@ -4,6 +4,9 @@ import {Marker, Popup} from "react-leaflet";
 import React, {useContext} from "react";
 import FilterListContext from "../../contexts/FilterListContext";
 import translationService from "../../services/translationService";
+import styled from "styled-components/macro";
+import mapCertificates from "../../services/mapCertificates";
+import ShowElementIconButton from "../../designElements/buttons/ShowElementIconButton";
 
 export default function MapMarkersForMap(ecoElements){
 
@@ -14,12 +17,37 @@ export default function MapMarkersForMap(ecoElements){
         ecoElements?.filter(element => (returnIfItemsGetsFiltered(element))).map((element) => (
                                 <Marker key={element.id} position={[element.lon, element.lat]}
                                         title={element.name}>
-                                    <Popup>
-                                        {element.name} <br/> {translationService(element.categorySub)} <br/> {element.address}
-                                    </Popup>
+                                    <StyledPopup>
+                                        <StyledPopupHeader>
+                                            {element.name}
+                                            <ShowElementIconButton elementId={element.id}/>
+                                        </StyledPopupHeader>
+                                        {translationService(element.categorySub)} <br/> {element.address} <br/>
+                                        <StyledMappedCertificates>
+                                            {mapCertificates(element, "small")}
+                                        </StyledMappedCertificates>
+                                    </StyledPopup>
                                 </Marker>
                 ))
     )
 }
 
 
+
+const StyledPopup = styled(Popup)`
+    font-size: 1.15em;
+`
+
+const StyledPopupHeader = styled.div`
+      font-weight: bold;
+      font-size: 1.15em;
+      display: grid;
+      grid-template-columns: auto auto;
+      grid-gap: 10px;
+      justify-content: left;
+      margin-right: 15px;
+`
+
+const StyledMappedCertificates = styled.div`
+    margin-top: 4px;
+`
