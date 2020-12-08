a296f3f31075771764a0de02c70a1bda11f8b1bc
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-12-08 15:43:16 +0100

    finalized the NewsfeedControllerTest.java.

--- backend/src/test/java/de/th/ecobobackend/controller/NewsfeedControllerTest.java
+++ backend/src/test/java/de/th/ecobobackend/controller/NewsfeedControllerTest.java
@@ -1,18 +1,14 @@
 package de.th.ecobobackend.controller;
-
 import de.th.ecobobackend.model.NewsfeedElement;
 import de.th.ecobobackend.model.UserProfile;
 import de.th.ecobobackend.model.dto.UserLoginDto;
 import de.th.ecobobackend.model.enums.NewsfeedType;
 import de.th.ecobobackend.mongoDB.NewsfeedMongoDB;
 import de.th.ecobobackend.mongoDB.UserProfileMongoDB;
-import de.th.ecobobackend.utils.IDUtils;
-import de.th.ecobobackend.utils.TimestampUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
@@ -21,12 +17,12 @@ import org.springframework.test.context.TestPropertySource;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {
@@ -36,12 +32,6 @@ class NewsfeedControllerTest {
 
     @LocalServerPort
     private int port;
-
-    @MockBean
-    private TimestampUtils mockedTimestampUtils;
-
-    @MockBean
-    private IDUtils mockedIdUtils;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -67,7 +57,8 @@ class NewsfeedControllerTest {
 
         userProfileMongoDB.deleteAll();
         String password = new BCryptPasswordEncoder().encode("Abc123");
-        userProfileMongoDB.save(new UserProfile("Wladimir Putin", password,
+
+        userProfileMongoDB.save(new UserProfile("Tobias", password,
                 Instant.now(), "20.10.2020"));
         userProfileMongoDB.save(new UserProfile("Donald Trump", password,
                 Instant.now(), "20.10.2020"));
@@ -76,24 +67,11 @@ class NewsfeedControllerTest {
 
     }
 
-    public List<NewsfeedElement> getNewsfeedElementList(){
-        NewsfeedElement newsfeedElement1 = NewsfeedElement.builder().type(NewsfeedType.ECOELEMENT_ADDED)
-                .linkedElement("1001").id("1001").message("Bioladen ist neu.").build();
-        NewsfeedElement newsfeedElement2 = NewsfeedElement.builder().type(NewsfeedType.ECOELEMENT_DELETED)
-                .linkedElement("1003").id("1002").message("Biokaufhaus wurde gelöscht.").build();
-        NewsfeedElement newsfeedElement3 = NewsfeedElement.builder().type(NewsfeedType.USER_REGISTRATION)
-                .linkedElement("Testuser").id("1003").message("Willkommen Testuser!").build();
-        return new ArrayList<NewsfeedElement>(List.of(newsfeedElement1,
-                newsfeedElement2, newsfeedElement3));
-    }
-
-    private final String newsfeedApiURL = "http://localhost:" + port + "/api/newsfeed";
-    private final String loginURL = "http://localhost:" + port + "/auth/login";
 
     private String login(){
 
         UserLoginDto userLoginDto = new UserLoginDto(
-                "Wladimir Putin",
+                "Tobias",
                 "Abc123"
         );
 
@@ -105,7 +83,6 @@ class NewsfeedControllerTest {
 
     private <T> HttpEntity<T> getValidAuthorizationEntity(T data) {
         String token = login();
-
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
         return new HttpEntity<T>(data,headers);
@@ -113,15 +90,42 @@ class NewsfeedControllerTest {
 
     @Test
     public void testGetNewsfeed() {
+        //Given
+        NewsfeedElement newsfeedElement1 = NewsfeedElement.builder().type(NewsfeedType.ECOELEMENT_ADDED)
+                .linkedElement("1001").id("1001").message("Bioladen ist neu.").build();
+        NewsfeedElement newsfeedElement2 = NewsfeedElement.builder().type(NewsfeedType.ECOELEMENT_DELETED)
+                .linkedElement("1003").id("1002").message("Biokaufhaus wurde gelöscht.").build();
+        NewsfeedElement newsfeedElement3 = NewsfeedElement.builder().type(NewsfeedType.USER_REGISTRATION)
+                .linkedElement("Testuser").id("1003").message("Willkommen Testuser!").build();
+        List<NewsfeedElement>newsfeedList = new ArrayList<>(List.of(newsfeedElement1,
+                newsfeedElement2, newsfeedElement3));
+
         //When
         HttpEntity<Void> entity = getValidAuthorizationEntity(null);
-        ResponseEntity<NewsfeedElement[]> response = restTemplate.exchange(newsfeedApiURL, HttpMethod.GET,
-                null, NewsfeedElement[].class);
-        List<NewsfeedElement> newsfeedList = getNewsfeedElementList();
+        ResponseEntity<NewsfeedElement[]> response = restTemplate.exchange("http://localhost:" + port + "/api/newsfeed", HttpMethod.GET,
+                entity, NewsfeedElement[].class);
 
         //Then
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertThat(response.getBody(), is(newsfeedList.toArray()));
+
+    }
+
+    @Test
+    public void testGetNewsfeedElements2() {
+        //Given
+        NewsfeedElement newsfeedElement3 = NewsfeedElement.builder().type(NewsfeedType.USER_REGISTRATION)
+                .linkedElement("Testuser").id("1003").message("Willkommen Testuser!").build();
+
+        //When
+        HttpEntity<Void> entity = getValidAuthorizationEntity(null);
+        ResponseEntity<NewsfeedElement[]> response = restTemplate.exchange("http://localhost:" + port + "/api/newsfeed/2", HttpMethod.GET,
+                entity, NewsfeedElement[].class);
+
+        //Then
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getBody().length, is(2));
+        assertThat(Arrays.stream(response.getBody()).findFirst(), is(Optional.of(newsfeedElement3)));
 
     }
 
